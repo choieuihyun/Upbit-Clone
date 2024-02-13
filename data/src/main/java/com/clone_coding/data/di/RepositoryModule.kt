d3a8a0a67b2e964fe548ecbb20a701b1ef0e78f7
choeuihyeon <amammmaa1@naver.com>
2024-02-13 23:02:49 +0900

    feature : 조금 늦은 TradeCenter Repository와 CoinInfo Repository 을 싱글톤 패턴으로 생성하는 RepositoryModule 구현

--- data/src/main/java/com/clone_coding/data/di/RepositoryModule.kt
+++ data/src/main/java/com/clone_coding/data/di/RepositoryModule.kt
@@ -1,4 +1,31 @@
 package com.clone_coding.data.di
 
+import com.clone_coding.data.datasource.remote_datasource.CoinInfoCoinTrendDatasource
+import com.clone_coding.data.datasource.remote_datasource.UpbitTradeCenterDatasource
+import com.clone_coding.data.repository.CoinInfoCoinTrendRepositoryImpl
+import com.clone_coding.data.repository.UpbitTradeCenterRepositoryImpl
+import com.clone_coding.domain.repository.CoinInfoCoinTrendRepository
+import com.clone_coding.domain.repository.UpbitTradeCenterRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideUpbitRepository(dataSource: UpbitTradeCenterDatasource) : UpbitTradeCenterRepository {
+        return UpbitTradeCenterRepositoryImpl(dataSource)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCoinInfoCoinTrendRepository(coinTrendDatasource: CoinInfoCoinTrendDatasource) : CoinInfoCoinTrendRepository {
+        return CoinInfoCoinTrendRepositoryImpl(coinTrendDatasource)
+    }
+
 }
