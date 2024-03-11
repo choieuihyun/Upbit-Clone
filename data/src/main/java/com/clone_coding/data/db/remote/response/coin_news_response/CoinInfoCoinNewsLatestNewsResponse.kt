8ba8202721883c6d718cda0e38314256700dea06
choeuihyeon <amammmaa1@naver.com>
2024-03-11 23:52:35 +0900

    feature : 코인 뉴스 data layer Response 구현

---
+++ data/src/main/java/com/clone_coding/data/db/remote/response/coin_news_response/CoinInfoCoinNewsLatestNewsResponse.kt
@@ -0,0 +1,13 @@
+package com.clone_coding.data.db.remote.response.coin_news_response
+
+// 사실 response는 아니라고 생각하는데 이름이 안떠오른다.
+data class CoinInfoCoinNewsLatestNewsResponse (
+
+    val newsTitle: String,
+
+    val newsHref: String,
+
+    val newsTime: String
+
+
+)
