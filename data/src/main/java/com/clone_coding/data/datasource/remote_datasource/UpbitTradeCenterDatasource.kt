9dde7ca833295183da0c579b444c2f54117d47ec
choeuihyeon <amammmaa1@naver.com>
2024-03-25 15:29:03 +0900

    feature : 거래소 마켓 Data Layer 구현

--- data/src/main/java/com/clone_coding/data/datasource/remote_datasource/UpbitTradeCenterDatasource.kt
+++ data/src/main/java/com/clone_coding/data/datasource/remote_datasource/UpbitTradeCenterDatasource.kt
@@ -3,21 +3,21 @@ package com.clone_coding.data.datasource.remote_datasource
 import android.util.Log
 import androidx.lifecycle.LiveData
 import com.clone_coding.data.db.remote.api.UpbitApi
-import com.clone_coding.data.db.remote.response.TradeCenterKRWTabInitializePriceResponse
-import com.clone_coding.data.db.remote.response.TradeCenterKRWTabMarketListResponse
-import com.clone_coding.data.di.YourWebSocketClient
-import com.clone_coding.data.di.YourWebSocketListener
+import com.clone_coding.data.db.remote.response.upbit_api_response.TradeCenterKRWTabInitializePriceResponse
+import com.clone_coding.data.db.remote.response.upbit_api_response.TradeCenterKRWTabMarketListResponse
+import com.clone_coding.data.di.WebSocketTicket
+import com.clone_coding.data.di.WebSocketClient
+import com.clone_coding.data.di.TradeCenterWebSocketListener
 import com.clone_coding.domain.error.NetworkErrorHandler
 import com.clone_coding.domain.error.NetworkResult
-import com.google.gson.Gson
 import okhttp3.Request
 import okhttp3.WebSocket
 import java.lang.Exception
 import javax.inject.Inject
 
 class UpbitTradeCenterDatasource @Inject constructor(
-    private val webSocketClient: YourWebSocketClient,
-    private val webSocketListener: YourWebSocketListener,
+    private val webSocketClient: WebSocketClient,
+    private val webSocketListener: TradeCenterWebSocketListener,
     private val webSocketUrl: String,
     private val upbitApi: UpbitApi, // REST API
     private val networkErrorHandler: NetworkErrorHandler
@@ -37,7 +37,6 @@ class UpbitTradeCenterDatasource @Inject constructor(
     val accTradePrice24H: LiveData<String>
         get() = webSocketListener.accTradePrice24H
 
-    private val gson = Gson()
     private lateinit var webSocket: WebSocket
 
     fun connectWebSocket() {
@@ -47,36 +46,8 @@ class UpbitTradeCenterDatasource @Inject constructor(
             .build()
 
         webSocket = webSocketClient.connectWebSocket(request, webSocketListener)
-        webSocket.send(createTicket())
+        webSocket.send(WebSocketTicket.createTicket())
 
-    }
-
-    data class Ticket(val ticket: String)
-    data class Type(val type: String, val codes: List<String>)
-
-    private fun createTicket(): String {
-        val ticket = Ticket("test")
-        val codeList = arrayListOf(
-            "KRW-BTC", "KRW-ETH", "KRW-NEO", "KRW-MTL", "KRW-XRP",
-            "KRW-ETC", "KRW-SNT", "KRW-WAVES", "KRW-XEM", "KRW-QTUM", "KRW-LSK", "KRW-STEEM",
-            "KRW-XLM", "KRW-ARDR", "KRW-ARK", "KRW-STORJ", "KRW-GRS", "KRW-ADA", "KRW-SBD",
-            "KRW-POWR", "KRW-BTG", "KRW-ICX", "KRW-EOS", "KRW-TRX", "KRW-SC", "KRW-ONT",
-            "KRW-ZIL", "KRW-POLYX", "KRW-ZRX", "KRW-LOOM", "KRW-BCH", "KRW-HIFI", "KRW-ONG",
-            "KRW-GAS", "KRW-UPP", "KRW-ELF", "KRW-KNC", "KRW-THETA", "KRW-QKC", "KRW-BTT",
-            "KRW-MOC", "KRW-TFUEL", "KRW-MANA", "KRW-ANKR", "KRW-AERGO", "KRW-ATOM", "KRW-TT",
-            "KRW-CRE", "KRW-MBL", "KRW-WAXP", "KRW-HBAR", "KRW-MED", "KRW-STPT", "KRW-ORBS",
-            "KRW-CHZ", "KRW-XTZ", "KRW-HIVE", "KRW-KAVA", "KRW-AHT", "KRW-LINK", "KRW-BORA",
-            "KRW-JST", "KRW-CRO", "KRW-SXP", "KRW-HUNT", "KRW-TON", "KRW-PLA", "KRW-DOT",
-            "KRW-MVL", "KRW-REI", "KRW-AQT", "KRW-STRAX", "KRW-GLM", "KRW-SSX", "KRW-META",
-            "KRW-FCT2", "KRW-CBK", "KRW-SAND", "KRW-HPO", "KRW-DOGE", "KRW-XEC", "KRW-SOL",
-            "KRW-MATIC", "KRW-AAVE", "KRW-1INCH", "KRW-FLOW", "KRW-AXS", "KRW-STX", "KRW-NEAR",
-            "KRW-ALGO", "KRW-T", "KRW-CELO", "KRW-GMT", "KRW-APT", "KRW-SHIB", "KRW-MASK",
-            "KRW-ARB", "KRW-EGLD", "KRW-SUI", "KRW-GRT", "KRW-BLUR", "KRW-IMX", "KRW-SEI",
-            "KRW-MINA", "KRW-CTC", "KRW-ASTR", "KRW-ID"
-        )
-        val type = Type("ticker", codeList)
-
-        return gson.toJson(arrayListOf(ticket, type))
     }
 
     // KRW Tab 데이터 뭉탱이 애초에 이 구조가 할 수가 없던거 아님? RESTful API는 한 번 받아오고 마는데 실시간 데이터를 이걸로 어떻게 하겠다는거?
@@ -104,6 +75,7 @@ class UpbitTradeCenterDatasource @Inject constructor(
     // 현재가 정보
     suspend fun getTradeCenterCurrentPrice(): NetworkResult<List<TradeCenterKRWTabInitializePriceResponse>?> {
 
+        // ssx 지움
         val response = upbitApi.getCurrentPriceInformation("KRW-BTC, KRW-ETH, KRW-NEO, KRW-MTL, KRW-XRP," +
                 " KRW-ETC, KRW-SNT, KRW-WAVES, KRW-XEM, KRW-QTUM, KRW-LSK," +
                 " KRW-STEEM, KRW-XLM, KRW-ARDR, KRW-ARK, KRW-STORJ, KRW-GRS," +
@@ -114,14 +86,14 @@ class UpbitTradeCenterDatasource @Inject constructor(
                 " KRW-ATOM, KRW-TT, KRW-CRE, KRW-MBL, KRW-WAXP, KRW-HBAR, KRW-MED," +
                 " KRW-STPT, KRW-ORBS, KRW-CHZ, KRW-XTZ, KRW-HIVE, KRW-KAVA, KRW-AHT," +
                 " KRW-LINK, KRW-BORA, KRW-JST, KRW-CRO, KRW-SXP, KRW-HUNT, KRW-TON," +
-                " KRW-PLA, KRW-DOT, KRW-MVL, KRW-AQT, KRW-STRAX, KRW-GLM, KRW-SSX," +
+                " KRW-PDA, KRW-DOT, KRW-MVL, KRW-AQT, KRW-STRAX, KRW-GLM, " +
                 " KRW-META, KRW-FCT2, KRW-CBK, KRW-SAND, KRW-HPO, KRW-DOGE, KRW-XEC," +
                 " KRW-SOL, KRW-MATIC, KRW-AAVE, KRW-1INCH, KRW-FLOW, KRW-AXS, KRW-STX," +
                 " KRW-NEAR, KRW-ALGO, KRW-T, KRW-CELO, KRW-GMT, KRW-APT, KRW-SHIB," +
                 " KRW-MASK, KRW-ARB, KRW-EGLD, KRW-SUI, KRW-GRT, KRW-BLUR, KRW-IMX," +
-                " KRW-SEI, KRW-MINA, KRW-CTC, KRW-ASTR, KRW-ID")
+                " KRW-SEI, KRW-MINA, KRW-CTC, KRW-ASTR, KRW-ID, KRW-PUNDIX")
 
-        Log.d("dataSourceCurrentPrice", response.body().toString())
+        Log.d("sdfsdf",response.body().toString())
 
         // 어차피 네트워크 연결 에러가 나면 여기서 에러가 나니까 Response도 같이 처리?
         return try {
