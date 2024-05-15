f20c79872aabadb70079167000c1fe78c9507eb8
choieuihyun <amammmaa1@naver.com>
2024-05-15 22:39:13 +0900

    refactor : 거래소 마켓 화면 에러처리 수정

--- data/src/main/java/com/clone_coding/data/datasource/remote_datasource/UpbitTradeCenterDatasource.kt
+++ data/src/main/java/com/clone_coding/data/datasource/remote_datasource/UpbitTradeCenterDatasource.kt
@@ -75,34 +75,37 @@ class UpbitTradeCenterDatasource @Inject constructor(
     // 현재가 정보
     suspend fun getTradeCenterCurrentPrice(): NetworkResult<List<TradeCenterKRWTabInitializePriceResponse>?> {
 
-        // ssx 지움, pda 지움
-        val response = upbitApi.getCurrentPriceInformation("KRW-BTC, KRW-ETH, KRW-NEO, KRW-MTL, KRW-XRP," +
-                " KRW-ETC, KRW-SNT, KRW-WAVES, KRW-XEM, KRW-QTUM, KRW-LSK," +
-                " KRW-STEEM, KRW-XLM, KRW-ARDR, KRW-ARK, KRW-STORJ, KRW-GRS," +
-                " KRW-ADA, KRW-SBD, KRW-POWR, KRW-BTG, KRW-ICX, KRW-EOS, KRW-TRX," +
-                " KRW-SC, KRW-ONT, KRW-ZIL, KRW-POLYX, KRW-ZRX, KRW-LOOM, KRW-BCH," +
-                " KRW-HIFI, KRW-ONG, KRW-GAS, KRW-UPP, KRW-ELF, KRW-KNC, KRW-THETA," +
-                " KRW-QKC, KRW-BTT, KRW-MOC, KRW-TFUEL, KRW-MANA, KRW-ANKR, KRW-AERGO," +
-                " KRW-ATOM, KRW-TT, KRW-CRE, KRW-MBL, KRW-WAXP, KRW-HBAR, KRW-MED," +
-                " KRW-STPT, KRW-ORBS, KRW-CHZ, KRW-XTZ, KRW-HIVE, KRW-KAVA, KRW-AHT," +
-                " KRW-LINK, KRW-BORA, KRW-JST, KRW-CRO, KRW-SXP, KRW-HUNT, KRW-TON," +
-                " KRW-DOT, KRW-MVL, KRW-AQT, KRW-STRAX, KRW-GLM, " +
-                " KRW-META, KRW-FCT2, KRW-CBK, KRW-SAND, KRW-HPO, KRW-DOGE, KRW-XEC," +
-                " KRW-SOL, KRW-MATIC, KRW-AAVE, KRW-1INCH, KRW-FLOW, KRW-AXS, KRW-STX," +
-                " KRW-NEAR, KRW-ALGO, KRW-T, KRW-CELO, KRW-GMT, KRW-APT, KRW-SHIB," +
-                " KRW-MASK, KRW-ARB, KRW-EGLD, KRW-SUI, KRW-GRT, KRW-BLUR, KRW-IMX," +
-                " KRW-SEI, KRW-MINA, KRW-CTC, KRW-ASTR, KRW-ID, KRW-PUNDIX, KRW-MNT")
+        try {
 
+            // ssx 지움, pda 지움
+            val response = upbitApi.getCurrentPriceInformation(
+                "KRW-BTC, KRW-ETH, KRW-NEO, KRW-MTL, KRW-XRP," +
+                        " KRW-ETC, KRW-SNT, KRW-WAVES, KRW-XEM, KRW-QTUM, KRW-LSK," +
+                        " KRW-STEEM, KRW-XLM, KRW-ARDR, KRW-ARK, KRW-STORJ, KRW-GRS," +
+                        " KRW-ADA, KRW-SBD, KRW-POWR, KRW-BTG, KRW-ICX, KRW-EOS, KRW-TRX," +
+                        " KRW-SC, KRW-ONT, KRW-ZIL, KRW-POLYX, KRW-ZRX, KRW-LOOM, KRW-BCH," +
+                        " KRW-HIFI, KRW-ONG, KRW-GAS, KRW-UPP, KRW-ELF, KRW-KNC, KRW-THETA," +
+                        " KRW-QKC, KRW-BTT, KRW-MOC, KRW-TFUEL, KRW-MANA, KRW-ANKR, KRW-AERGO," +
+                        " KRW-ATOM, KRW-TT, KRW-CRE, KRW-MBL, KRW-WAXP, KRW-HBAR, KRW-MED," +
+                        " KRW-STPT, KRW-ORBS, KRW-CHZ, KRW-XTZ, KRW-HIVE, KRW-KAVA, KRW-AHT," +
+                        " KRW-LINK, KRW-BORA, KRW-JST, KRW-CRO, KRW-SXP, KRW-HUNT, KRW-TON," +
+                        " KRW-DOT, KRW-MVL, KRW-AQT, KRW-STRAX, KRW-GLM, " +
+                        " KRW-META, KRW-FCT2, KRW-CBK, KRW-SAND, KRW-HPO, KRW-DOGE, KRW-XEC," +
+                        " KRW-SOL, KRW-MATIC, KRW-AAVE, KRW-1INCH, KRW-FLOW, KRW-AXS, KRW-STX," +
+                        " KRW-NEAR, KRW-ALGO, KRW-T, KRW-CELO, KRW-GMT, KRW-APT, KRW-SHIB," +
+                        " KRW-MASK, KRW-ARB, KRW-EGLD, KRW-SUI, KRW-GRT, KRW-BLUR, KRW-IMX," +
+                        " KRW-SEI, KRW-MINA, KRW-CTC, KRW-ASTR, KRW-ID, KRW-PUNDIX, KRW-MNT"
+            )
+            Log.d("sdfsdf", response.body().toString())
+            return NetworkResult.Success(response.body())
 
-
-        // 어차피 네트워크 연결 에러가 나면 여기서 에러가 나니까 Response도 같이 처리?
-        return try {
-            Log.d("sdfsdf",response.body().toString())
-            NetworkResult.Success(response.body())
         } catch (e: Exception) {
             val errorType = networkErrorHandler.errorMessage(e)
-            NetworkResult.Error(errorType)
+            Log.d("sdfsdfError", errorType.toString())
+            return NetworkResult.Error(errorType)
         }
+
+
 
     }
 
